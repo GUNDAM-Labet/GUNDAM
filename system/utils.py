d8f92d9dff78c1bb373987e650c92b8a7845a9f9
Jinjiarui <jinjiarui97@gmail.com>
2023-06-16 15:13:20 +0000

    minor

--- system/utils.py
+++ system/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from typing import List, Optional, Iterator, Dict
+from typing import Optional
 from dataclasses import dataclass
 
 IO_SEP_TOKEN = "|||"
@@ -15,11 +15,3 @@ class Unit:
     source_emb: Optional[np.ndarray] = None
     target_emb: Optional[np.ndarray] = None
     priority_level: int
-
-
-def trim_batch_data(input_ids, pad_token_id, attention_mask=None): # remove columns by pad_token_id
-    keep_column_mask = input_ids.ne(pad_token_id).any(dim=0)
-    if attention_mask:
-        return (input_ids[:, keep_column_mask], attention_mask[:, keep_column_mask])
-    else:
-        return input_ids[:, keep_column_mask]    
